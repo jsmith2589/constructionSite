129eed16aa32466cdf4760ab0d635f3b2710b2cd
jsmith2589 <65322603+jsmith2589@users.noreply.github.com>
2022-04-21 20:50:27 -0400

    Add files via upload

---
+++ yates practice/index.js
@@ -0,0 +1,17 @@
+//Main slide show
+var slidePosition = 0
+SlideShow()
+
+function SlideShow() {
+  var i
+  var slides = document.getElementsByClassName("containers")
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slidePosition++
+  if (slidePosition > slides.length) {
+    slidePosition = 1
+  }
+  slides[slidePosition - 1].style.display = "block"
+  setTimeout(SlideShow, 3000) // Change image every 2 seconds
+}
